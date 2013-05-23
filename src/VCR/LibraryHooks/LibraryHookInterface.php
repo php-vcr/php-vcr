87a1c13be0895ccd32e88be15b3e45a5ef8fcfee
Adrian Philipp <mail@adrian-philipp.com>
2013-05-23 13:58:44 +0700

    Adds comments to LibraryHookInterface.

--- src/VCR/LibraryHooks/LibraryHookInterface.php
+++ src/VCR/LibraryHooks/LibraryHookInterface.php
@@ -7,11 +7,31 @@ namespace VCR\LibraryHooks;
  */
 interface LibraryHookInterface
 {
+    /**
+     * @var string Enabled status for a hook.
+     */
     const ENABLED = 'ENABLED';
+
+    /**
+     * @var string Disabled status for a hook.
+     */
     const DISABLED = 'DISABLED';
 
+    /**
+     * Enables library hook which means that all of this library
+     * http interactions are intercepted.
+     *
+     * @param \Closure Callback which will be called when a request is intercepted.
+     * @return void
+     */
     public function enable(\Closure $handleRequestCallback);
 
+    /**
+     * Disables library hook, so no http interactions of
+     * this library are intercepted anymore.
+     *
+     * @return void
+     */
     public function disable();
 
 }
