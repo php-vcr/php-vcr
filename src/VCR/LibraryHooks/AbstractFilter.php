03cea9171639225a44194e18146efb22cf6c6ee1
Bastian Feder <github@bastian-feder.de>
2013-10-10 20:06:29 +0200

    Implemented: added interface and abstract class for the PHP_User_Filter

---
+++ src/VCR/LibraryHooks/AbstractFilter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace VCR\LibraryHooks;
+
+
+abstract class AbstractFilter extends \PHP_User_Filter implements FilterInterface
+{
+
+    protected $isRegistered = false;
+
+    /**
+     * @return bool true on success or false on failure.
+     */
+    public function register()
+    {
+        if (!$this->isRegistered) {
+
+            $this->isRegistered = stream_filter_register(self::NAME, __CLASS__);
+        }
+
+        return $this->isRegistered;
+    }
+
+    /**
+     * @param resource $in
+     * @param resource $out
+     * @param int $consumed
+     * @param bool $closing
+     *
+     * @return int PSFS_PASS_ON
+     *
+     * @link http://www.php.net/manual/en/php-user-filter.filter.php
+     */
+    public function filter($in, $out, &$consumed, $closing)
+    {
+        while ($bucket = stream_bucket_make_writeable($in)) {
+            $bucket->data = $this->transformCode($bucket->data);
+            $consumed += $bucket->datalen;
+            stream_bucket_append($out, $bucket);
+        }
+
+        return PSFS_PASS_ON;
+    }
+
+    /**
+     * Transcodes the provided data to whatever.
+     *
+     * @param string $code
+     *
+     * @return string
+     */
+    abstract protected function transformCode($code);
+}
