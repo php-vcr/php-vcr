4519b44952fb3a1e7d8bc7ded1d8bc2af47618d7
Adrian Philipp <mail@adrian-philipp.com>
2013-10-10 23:55:21 +0200

    Fixed: Changes tests to add new dependencies to CurlRewrite library hook.

--- tests/VCR/LibraryHooks/CurlRewriteTest.php
+++ tests/VCR/LibraryHooks/CurlRewriteTest.php
@@ -2,7 +2,11 @@
 
 namespace VCR\LibraryHooks;
 
-use \VCR\Response;
+use VCR\Response;
+use VCR\Configuration;
+use VCR\LibraryHooks\CurlRewrite\Filter;
+use VCR\Util\StreamProcessor;
+
 
 /**
  * Test if intercepting http/https using curl rewrite works.
@@ -11,20 +15,25 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
 {
     public $expected = 'example response body';
 
+    public function setup()
+    {
+        $this->config = new Configuration();
+        $this->curlHook = new CurlRewrite(new Filter(), new StreamProcessor($this->config));
+    }
+
     /**
      * @group runkit
      */
     public function testShouldInterceptCallWhenEnabled()
     {
-        $curlHook = new CurlRewrite();
-        $curlHook->enable($this->getTestCallback());
+        $this->curlHook->enable($this->getTestCallback());
 
         $ch = curl_init('http://127.0.0.1/');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $actual = curl_exec($ch);
         curl_close($ch);
 
-        $curlHook->disable();
+        $this->curlHook->disable();
         $this->assertEquals($this->expected, $actual, 'Response was not returned.');
     }
 
@@ -35,7 +44,7 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
     {
         $this->markTestSkipped('Uses internet connection, find another way to test this.');
         $testClass = $this;
-        $curlHook = new CurlRewrite();
+        $this->curlHook = new CurlRewrite();
 
         $ch = curl_init('http://127.0.0.1/');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -49,11 +58,10 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
     public function testShouldNotInterceptCallWhenDisabled()
     {
         $testClass = $this;
-        $curlHook = new CurlRewrite();
-        $curlHook->enable(function($request) use($testClass) {
+        $this->curlHook->enable(function($request) use($testClass) {
             $testClass->fail('This request should not have been intercepted.');
         });
-        $curlHook->disable();
+        $this->curlHook->disable();
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'http://127.0.0.1/');
@@ -67,8 +75,7 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
      */
     public function testShouldWriteFileOnFileDownload()
     {
-        $curlHook = new CurlRewrite();
-        $curlHook->enable($this->getTestCallback());
+        $this->curlHook->enable($this->getTestCallback());
 
         $ch = curl_init('https://127.0.0.1/');
         $fp = fopen('php://temp/test_file', 'w');
@@ -79,7 +86,7 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
         $actual = fread($fp, 1024);
         fclose($fp);
 
-        $curlHook->disable();
+        $this->curlHook->disable();
         $this->assertEquals($this->expected, $actual, 'Response was not written in file.');
     }
 
@@ -88,8 +95,7 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
      */
     public function testShouldEchoResponseIfReturnTransferFalse()
     {
-        $curlHook = new CurlRewrite();
-        $curlHook->enable($this->getTestCallback());
+        $this->curlHook->enable($this->getTestCallback());
 
         $ch = curl_init('http://127.0.0.1/');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
@@ -99,7 +105,7 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
         ob_end_clean();
         curl_close($ch);
 
-        $curlHook->disable();
+        $this->curlHook->disable();
         $this->assertEquals($this->expected, $actual, 'Response was not written on stdout.');
     }
 
@@ -109,8 +115,7 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
     public function testShouldPostFieldsAsString()
     {
         $testClass = $this;
-        $curlHook = new CurlRewrite();
-        $curlHook->enable(function($request) use($testClass) {
+        $this->curlHook->enable(function($request) use($testClass) {
             $testClass->assertEquals(
                 array('para1' => 'val1', 'para2' => 'val2'),
                 $request->getPostFields()->getAll(),
@@ -123,7 +128,7 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
         curl_setopt($ch, CURLOPT_POSTFIELDS, 'para1=val1&para2=val2');
         curl_exec($ch);
         curl_close($ch);
-        $curlHook->disable();
+        $this->curlHook->disable();
     }
 
     /**
@@ -132,8 +137,7 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
     public function testShouldPostFieldsAsArray()
     {
         $testClass = $this;
-        $curlHook = new CurlRewrite;
-        $curlHook->enable(function($request) use($testClass) {
+        $this->curlHook->enable(function($request) use($testClass) {
             $testClass->assertEquals(
                 array('para1' => 'val1', 'para2' => 'val2'),
                 $request->getPostFields()->getAll(),
@@ -146,7 +150,7 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
         curl_setopt($ch, CURLOPT_POSTFIELDS, array('para1' => 'val1', 'para2' => 'val2'));
         curl_exec($ch);
         curl_close($ch);
-        $curlHook->disable();
+        $this->curlHook->disable();
     }
 
     /**
@@ -154,8 +158,7 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
      */
     public function testShouldReturnCurlInfoStatusCode()
     {
-        $curlHook = new CurlRewrite();
-        $curlHook->enable($this->getTestCallback());
+        $this->curlHook->enable($this->getTestCallback());
 
         $ch = curl_init('http://127.0.0.1');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -163,7 +166,7 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
         curl_close($ch);
 
         $this->assertEquals(200, curl_getinfo($ch, CURLINFO_HTTP_CODE), 'HTTP status not set.');
-        $curlHook->disable();
+        $this->curlHook->disable();
     }
 
     /**
@@ -171,8 +174,7 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
      */
     public function testShouldReturnCurlInfoAll()
     {
-        $curlHook = new CurlRewrite();
-        $curlHook->enable($this->getTestCallback());
+        $this->curlHook->enable($this->getTestCallback());
 
         $ch = curl_init('http://127.0.0.1');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -181,7 +183,7 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
 
         $this->assertTrue(is_array(curl_getinfo($ch)), 'curl_getinfo() should return an array.');
         $this->assertEquals(19, count(curl_getinfo($ch)), 'curl_getinfo() should return 19 values.');
-        $curlHook->disable();
+        $this->curlHook->disable();
     }
 
     /**
@@ -189,9 +191,8 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
      */
     public function testShouldNotThrowErrorWhenDisabledTwice()
     {
-        $curlHook = new CurlRewrite();
-        $curlHook->disable();
-        $curlHook->disable();
+        $this->curlHook->disable();
+        $this->curlHook->disable();
     }
 
     /**
@@ -199,9 +200,8 @@ class CurlRewriteTest extends \PHPUnit_Framework_TestCase
      */
     public function testShouldNotThrowErrorWhenEnabledTwice()
     {
-        $curlHook = new CurlRewrite();
-        $curlHook->enable($this->getTestCallback());
-        $curlHook->enable($this->getTestCallback());
+        $this->curlHook->enable($this->getTestCallback());
+        $this->curlHook->enable($this->getTestCallback());
     }
 
     /**
