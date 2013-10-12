dbf88887c161b24b50116ca0c3279a31beb9f652
Bastian Feder <github@bastian-feder.de>
2013-10-12 13:34:33 +0200

    Implemented transformCode() in Soap Filter to make sure the intercetor gets called

--- src/VCR/LibraryHooks/Soap/Filter.php
+++ src/VCR/LibraryHooks/Soap/Filter.php
@@ -8,13 +8,23 @@ class Filter extends AbstractFilter
 {
     const NAME = 'vcr_soap';
 
+    private static $replacements = array(
+        'new \VCR\LibraryHooks\Soap(',
+        'extends \VCR\LibraryHooks\Soap',
+    );
+
+    private static $patterns = array(
+        '@new\s+\\\?SoapClient\W*\(@i',
+        '@extends\s+SoapClient\W*@i',
+    );
+
     /**
      * @param string $code
      *
      * @return mixed
      */
-    public function transformCode($code)
+    protected function transformCode($code)
     {
-        return $code;
+        return preg_replace(self::$patterns, self::$replacements, $code);
     }
 }
---
+++ tests/VCR/LibraryHooks/Soap/FilterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace VCR\LibraryHooks\Soap;
+
+
+use VCR\VCR_TestCase;
+
+
+class FilterTest extends VCR_TestCase
+{
+    /**
+     * @dataProvider codeSnippetProvider
+     */
+    public function testTransformCode($expected, $code)
+    {
+        $filter = $this->getProxyBuilder('\VCR\LibraryHooks\Soap\Filter')
+            ->setMethods(array('transformCode'))
+            ->getProxy();
+
+        $this->assertEquals($expected, $filter->transformCode($code));
+    }
+
+    public function codeSnippetProvider()
+    {
+        return array(
+            'new \SoapClient' => array(
+                'new \VCR\LibraryHooks\Soap(',
+                'new \SoapClient('
+            ),
+            'new SoapClient' => array(
+                'new \VCR\LibraryHooks\Soap(',
+                'new SoapClient('
+            ),
+            'new SoapClientExtended' => array(
+                'new SoapClientExtended(',
+                'new SoapClientExtended('
+            ),
+            'new \SoapClientExtended' => array(
+                'new \SoapClientExtended(',
+                'new \SoapClientExtended('
+            ),
+        );
+    }
+}
