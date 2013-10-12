ba510b5bf2df9c7f84a1530714cf79793677ddf1
Bastian Feder <github@bastian-feder.de>
2013-10-12 10:50:30 +0200

    Refactored: CurlRewrite\Filter now uses regexp to find curl_* functions to be replaced

--- src/VCR/LibraryHooks/CurlRewrite/Filter.php
+++ src/VCR/LibraryHooks/CurlRewrite/Filter.php
@@ -9,14 +9,25 @@ class Filter extends AbstractFilter
     const NAME = 'vcr_curl_rewrite';
 
     private static $replacements = array(
-        'curl_init('    => '\VCR\LibraryHooks\CurlRewrite::curl_init(',
-        'curl_exec('    => '\VCR\LibraryHooks\CurlRewrite::curl_exec(',
-        'curl_getinfo(' => '\VCR\LibraryHooks\CurlRewrite::curl_getinfo(',
-        'curl_setopt('  => '\VCR\LibraryHooks\CurlRewrite::curl_setopt(',
-        'curl_multi_add_handle('    => '\VCR\LibraryHooks\CurlRewrite::curl_multi_add_handle(',
-        'curl_multi_remove_handle(' => '\VCR\LibraryHooks\CurlRewrite::curl_multi_remove_handle(',
-        'curl_multi_exec('          => '\VCR\LibraryHooks\CurlRewrite::curl_multi_exec(',
-        'curl_multi_info_read('     => '\VCR\LibraryHooks\CurlRewrite::curl_multi_info_read('
+        '\VCR\LibraryHooks\CurlRewrite::curl_init(',
+        '\VCR\LibraryHooks\CurlRewrite::curl_exec(',
+        '\VCR\LibraryHooks\CurlRewrite::curl_getinfo(',
+        '\VCR\LibraryHooks\CurlRewrite::curl_setopt(',
+        '\VCR\LibraryHooks\CurlRewrite::curl_multi_add_handle(',
+        '\VCR\LibraryHooks\CurlRewrite::curl_multi_remove_handle(',
+        '\VCR\LibraryHooks\CurlRewrite::curl_multi_exec(',
+        '\VCR\LibraryHooks\CurlRewrite::curl_multi_info_read('
+    );
+
+    private static $patterns = array(
+        '@curl_init\s*\(@i',
+        '@curl_exec\s*\(@i',
+        '@curl_getinfo\s*\(@i',
+        '@curl_setopt\s*\(@i',
+        '@curl_multi_add_handle\s*\(@i',
+        '@curl_multi_remove_handle\s*\(@i',
+        '@curl_multi_exec\s*\(@i',
+        '@curl_multi_info_read\s*\(@i',
     );
 
     /**
@@ -26,10 +37,6 @@ class Filter extends AbstractFilter
      */
     protected function transformCode($code)
     {
-        return str_replace(
-            array_keys(self::$replacements),
-            array_values(self::$replacements),
-            $code
-        );
+        return preg_replace(self::$patterns, self::$replacements, $code);
     }
 }
--- tests/VCR/VCR_TestCase.php
+++ tests/VCR/VCR_TestCase.php
@@ -1,11 +1,18 @@
 <?php
 namespace VCR;
 
+use lapistano\ProxyObject\ProxyBuilder;
 use VCR\LibraryHooks\CurlRunkit;
 
 
 class VCR_TestCase extends \PHPUnit_Framework_TestCase
 {
+
+    protected function getProxyBuilder($className)
+    {
+        return new ProxyBuilder($className);
+    }
+
     protected function skipTestIfRunkitUnavailable()
     {
         try{
