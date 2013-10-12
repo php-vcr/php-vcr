ca74a5312e5f55e3ae3d2a468db3bf1d3efd4eca
Adrian Philipp <mail@adrian-philipp.com>
2013-10-12 16:42:38 +0200

    Cleanup: Removed unused SoapTests.

--- tests/VCR/LibraryHooks/SoapTest.php
+++ tests/VCR/LibraryHooks/SoapTest.php
@@ -32,63 +32,6 @@ class SoapTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($this->expected, $actual, 'Response was not returned.');
     }
 
-    public function testShouldNotInterceptCallWhenNotEnabled()
-    {
-        $this->markTestSkipped('uses internet connection, howto?');
-        $this->soapHook->disable();
-
-        $client = new \SoapClient('http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL', array('soap_version'   => SOAP_1_2));
-        $response = $client->GetCityWeatherByZIP(array('ZIP' => '10013'));
-
-        $this->soapHook->disable();
-        $this->assertEquals($this->expected, $actual, 'Response was not returned.');
-    }
-
-    public function testShouldNotInterceptCallWhenDisabled()
-    {
-        $this->markTestSkipped('not yet done');
-    }
-
-    public function testShouldWriteFileOnFileDownload()
-    {
-        $this->markTestSkipped('not yet done');
-    }
-
-    public function testShouldEchoResponseIfReturnTransferFalse()
-    {
-        $this->markTestSkipped('not yet done');
-    }
-
-    public function testShouldPostFieldsAsString()
-    {
-        $this->markTestSkipped('not yet done');
-    }
-
-    public function testShouldPostFieldsAsArray()
-    {
-        $this->markTestSkipped('not yet done');
-    }
-
-    public function testShouldReturnCurlInfoStatusCode()
-    {
-        $this->markTestSkipped('not yet done');
-    }
-
-    public function testShouldReturnCurlInfoAll()
-    {
-        $this->markTestSkipped('not yet done');
-    }
-
-    public function testShouldNotThrowErrorWhenDisabledTwice()
-    {
-        $this->markTestSkipped('not yet done');
-    }
-
-    public function testShouldNotThrowErrorWhenEnabledTwice()
-    {
-        $this->markTestSkipped('not yet done');
-    }
-
     /**
      * @return \callable
      */
