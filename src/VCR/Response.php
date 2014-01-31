277c2f59ae0698391921fa95f0611529166eec24
Adrian Philipp <mail@adrian-philipp.com>
2014-01-31 21:55:18 +0100

    Cleanup: Documents Response and adds tests.

--- src/VCR/Response.php
+++ src/VCR/Response.php
@@ -2,33 +2,44 @@
 
 namespace VCR;
 
+/**
+ * Encapsulates a HTTP response.
+ */
 class Response extends \Guzzle\Http\Message\Response
 {
+    /**
+     * Returns an array representation of this Response.
+     *
+     * @return array Arrey representation of this Request.
+     */
     public function toArray()
     {
         $body = (string) $this->getBody(true);
 
         // Base64 encode when binary
         if (strpos($this->getContentType(), 'application/x-gzip') !== false) {
-           $body = base64_encode($body);
+            $body = base64_encode($body);
         }
 
-        return array_filter(array(
-            'status'    => $this->getStatusCode(),
-            'headers'   => $this->getHeaders(),
-            'body'      => $body,
-            'curl_info' => $this->getInfo()
-        ));
+        return array_filter(
+            array(
+                'status'    => $this->getStatusCode(),
+                'headers'   => $this->getHeaders(),
+                'body'      => $body,
+                'curl_info' => $this->getInfo()
+            )
+        );
     }
 
-    public function getHeaders($asObjects = false)
+    /**
+     * Returns a list of headers as key/value pairs.
+     *
+     * @return array List of headers as key/value pairs.
+     */
+    public function getHeaders()
     {
-        if ($asObjects === true) {
-            return parent::getHeaders($asObjects);
-        }
-
         $headers = array();
-        foreach (parent::getHeaders() as $key => $header) {
+        foreach (parent::getHeaders() as $header) {
             $values = $header->toArray();
             $headers[$header->getName()] = $values[0];
         }
@@ -36,6 +47,12 @@ class Response extends \Guzzle\Http\Message\Response
         return $headers;
     }
 
+    /**
+     * Creates a new Response from a specified array.
+     *
+     * @param  array  $response Array representation of a Response.
+     * @return Response A new Response from a specified array
+     */
     public static function fromArray(array $response)
     {
         $body = isset($response['body']) ? $response['body'] : null;
