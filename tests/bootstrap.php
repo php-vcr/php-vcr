7554688ec4290d266ea3971fa9c091f2ad653c16
Adrian Philipp <mail@adrian-philipp.com>
2014-01-31 18:06:05 +0100

    Cleanup: Removes unnecessary loading of testcase (was deleted).

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,13 +9,7 @@ if (!file_exists(__DIR__ . "/../vendor/autoload.php")) {
     );
 }
 
-$loader = include __DIR__ . '/../vendor/autoload.php';
-
-$loader->addClassMap(
-    array(
-        'VCR\\VCR_TestCase' => __DIR__ . '/VCR/VCR_TestCase.php',
-    )
-);
+require_once __DIR__ . '/../vendor/autoload.php';
 
 \VCR\VCR::configure()
     ->enableLibraryHooks(array('curl', 'soap'))
