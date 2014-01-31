a76eef48a9b7d4d52a0196d23f2bdd4a39f50135
Adrian Philipp <mail@adrian-philipp.com>
2014-01-31 18:07:19 +0100

    Cleanup: Removes obsolete configuration.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,8 +11,5 @@ if (!file_exists(__DIR__ . "/../vendor/autoload.php")) {
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-\VCR\VCR::configure()
-    ->enableLibraryHooks(array('curl', 'soap'))
-    ->setBlackList(array('Soap/FilterTest'));
 \VCR\VCR::turnOn();
 \VCR\VCR::turnOff();
