02c4929d3b891b42303a8233193fd07a74848b06
Adrian Philipp <mail@adrian-philipp.com>
2014-02-12 20:19:43 +0100

    Fixed: CurlFilter doesn't overwrite method calls to curl-like named methods.

--- src/VCR/Filter/CurlFilter.php
+++ src/VCR/Filter/CurlFilter.php
@@ -6,28 +6,16 @@ class CurlFilter extends AbstractFilter
 {
     const NAME = 'vcr_curl';
 
-    private static $replacements = array(
-        '\VCR\LibraryHooks\CurlHook::curl_init(',
-        '\VCR\LibraryHooks\CurlHook::curl_exec(',
-        '\VCR\LibraryHooks\CurlHook::curl_getinfo(',
-        '\VCR\LibraryHooks\CurlHook::curl_setopt(',
-        '\VCR\LibraryHooks\CurlHook::curl_setopt_array(',
-        '\VCR\LibraryHooks\CurlHook::curl_multi_add_handle(',
-        '\VCR\LibraryHooks\CurlHook::curl_multi_remove_handle(',
-        '\VCR\LibraryHooks\CurlHook::curl_multi_exec(',
-        '\VCR\LibraryHooks\CurlHook::curl_multi_info_read('
-    );
-
     private static $patterns = array(
-        '@curl_init\s*\(@i',
-        '@curl_exec\s*\(@i',
-        '@curl_getinfo\s*\(@i',
-        '@curl_setopt\s*\(@i',
-        '@curl_setopt_array\s*\(@i',
-        '@curl_multi_add_handle\s*\(@i',
-        '@curl_multi_remove_handle\s*\(@i',
-        '@curl_multi_exec\s*\(@i',
-        '@curl_multi_info_read\s*\(@i',
+        '/(?<!::|->)curl_init\s*\(/i'                => '\VCR\LibraryHooks\CurlHook::curl_init(',
+        '/(?<!::|->)curl_exec\s*\(/i'                => '\VCR\LibraryHooks\CurlHook::curl_exec(',
+        '/(?<!::|->)curl_getinfo\s*\(/i'             => '\VCR\LibraryHooks\CurlHook::curl_getinfo(',
+        '/(?<!::|->)curl_setopt\s*\(/i'              => '\VCR\LibraryHooks\CurlHook::curl_setopt(',
+        '/(?<!::|->)curl_setopt_array\s*\(/i'        => '\VCR\LibraryHooks\CurlHook::curl_setopt_array(',
+        '/(?<!::|->)curl_multi_add_handle\s*\(/i'    => '\VCR\LibraryHooks\CurlHook::curl_multi_add_handle(',
+        '/(?<!::|->)curl_multi_remove_handle\s*\(/i' => '\VCR\LibraryHooks\CurlHook::curl_multi_remove_handle(',
+        '/(?<!::|->)curl_multi_exec\s*\(/i'          => '\VCR\LibraryHooks\CurlHook::curl_multi_exec(',
+        '/(?<!::|->)curl_multi_info_read\s*\(/i'     => '\VCR\LibraryHooks\CurlHook::curl_multi_info_read('
     );
 
     /**
@@ -37,6 +25,6 @@ class CurlFilter extends AbstractFilter
      */
     protected function transformCode($code)
     {
-        return preg_replace(self::$patterns, self::$replacements, $code);
+        return preg_replace(array_keys(self::$patterns), array_values(self::$patterns), $code);
     }
 }
--- tests/VCR/Filter/CurlFilterTest.php
+++ tests/VCR/Filter/CurlFilterTest.php
@@ -30,6 +30,25 @@ class CurlFilterTest extends \PHPUnit_Framework_TestCase
             array('\VCR\LibraryHooks\CurlHook::curl_multi_remove_handle(', 'curl_multi_remove_handle('),
             array('\VCR\LibraryHooks\CurlHook::curl_multi_exec(', 'curl_multi_exec('),
             array('\VCR\LibraryHooks\CurlHook::curl_multi_info_read(', 'curl_multi_info_read('),
+
+            array('SomeClass::CURL_INIT (', 'SomeClass::CURL_INIT ('),
+            array('SomeClass::curl_exec(', 'SomeClass::curl_exec('),
+            array('SomeClass::curl_getinfo(', 'SomeClass::curl_getinfo('),
+            array('SomeClass::curl_setopt(', 'SomeClass::curl_setopt('),
+            array('SomeClass::curl_multi_add_handle(', 'SomeClass::curl_multi_add_handle('),
+            array('SomeClass::curl_multi_remove_handle(', 'SomeClass::curl_multi_remove_handle('),
+            array('SomeClass::curl_multi_exec(', 'SomeClass::curl_multi_exec('),
+            array('SomeClass::curl_multi_info_read(', 'SomeClass::curl_multi_info_read('),
+
+            array('$object->CURL_INIT (', '$object->CURL_INIT ('),
+            array('$object->curl_exec(', '$object->curl_exec('),
+            array('$object->curl_getinfo(', '$object->curl_getinfo('),
+            array('$object->curl_setopt(', '$object->curl_setopt('),
+            array('$object->curl_multi_add_handle(', '$object->curl_multi_add_handle('),
+            array('$object->curl_multi_remove_handle(', '$object->curl_multi_remove_handle('),
+            array('$object->curl_multi_exec(', '$object->curl_multi_exec('),
+            array('$object->curl_multi_info_read(', '$object->curl_multi_info_read('),
+
         );
     }
 }
