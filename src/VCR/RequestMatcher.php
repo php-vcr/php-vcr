fcac43c5308281a01d7553f8400c5723acc71e33
Adrian Philipp <mail@adrian-philipp.com>
2014-02-20 18:22:14 +0100

    Cleanup: Documentes RequestMatcher class.

--- src/VCR/RequestMatcher.php
+++ src/VCR/RequestMatcher.php
@@ -2,14 +2,32 @@
 
 namespace VCR;
 
+/**
+ * Collection of matcher methods to match two requests.
+ */
 class RequestMatcher
 {
-
+    /**
+     * Returns true if the method of both specified requests match.
+     *
+     * @param  Request $first  First request to match.
+     * @param  Request $second Second request to match.
+     *
+     * @return boolean True if the method of both specified requests match.
+     */
     public static function matchMethod(Request $first, Request $second)
     {
         return $first->getMethod() == $second->getMethod();
     }
 
+    /**
+     * Returns true if the url of both specified requests match.
+     *
+     * @param  Request $first  First request to match.
+     * @param  Request $second Second request to match.
+     *
+     * @return boolean True if the url of both specified requests match.
+     */
     public static function matchUrl(Request $first, Request $second)
     {
         if (null !== $first->getPath()) {
@@ -23,6 +41,14 @@ class RequestMatcher
         return true;
     }
 
+    /**
+     * Returns true if the host of both specified requests match.
+     *
+     * @param  Request $first  First request to match.
+     * @param  Request $second Second request to match.
+     *
+     * @return boolean True if the host of both specified requests match.
+     */
     public static function matchHost(Request $first, Request $second)
     {
         if (null !== $first->getHost()
@@ -33,6 +59,14 @@ class RequestMatcher
         return true;
     }
 
+    /**
+     * Returns true if the headers of both specified requests match.
+     *
+     * @param  Request $first  First request to match.
+     * @param  Request $second Second request to match.
+     *
+     * @return boolean True if the headers of both specified requests match.
+     */
     public static function matchHeaders(Request $first, Request $second)
     {
         $firstHeaders = $first->getHeaders();
@@ -46,6 +80,14 @@ class RequestMatcher
         return true;
     }
 
+    /**
+     * Returns true if the body of both specified requests match.
+     *
+     * @param  Request $first  First request to match.
+     * @param  Request $second Second request to match.
+     *
+     * @return boolean True if the body of both specified requests match.
+     */
     public static function matchBody(Request $first, Request $second)
     {
         if (null !== $first->getBody() && (string) $first->getBody() != (string) $second->getBody() ) {
@@ -55,6 +97,14 @@ class RequestMatcher
         return true;
     }
 
+    /**
+     * Returns true if the post fields of both specified requests match.
+     *
+     * @param  Request $first  First request to match.
+     * @param  Request $second Second request to match.
+     *
+     * @return boolean True if the post fields of both specified requests match.
+     */
     public static function matchPostFields(Request $first, Request $second)
     {
         if (null !== $first->getPostFields()->toArray()
@@ -65,6 +115,14 @@ class RequestMatcher
         return true;
     }
 
+    /**
+     * Returns true if the query string of both specified requests match.
+     *
+     * @param  Request $first  First request to match.
+     * @param  Request $second Second request to match.
+     *
+     * @return boolean True if the query string of both specified requests match.
+     */
     public static function matchQueryString(Request $first, Request $second)
     {
         if (null !== $first->getQuery(true) && $first->getQuery(true) != $second->getQuery(true)) {
