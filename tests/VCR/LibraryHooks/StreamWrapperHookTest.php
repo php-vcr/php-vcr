12d326e375153d46abb7233d06b643a4171334e4
Adrian Philipp <mail@adrian-philipp.com>
2014-02-21 16:23:54 +0100

    Cleanup: Adds tests for StreamWrapperHook::stream_seek().

--- tests/VCR/LibraryHooks/StreamWrapperHookTest.php
+++ tests/VCR/LibraryHooks/StreamWrapperHookTest.php
@@ -2,6 +2,8 @@
 
 namespace VCR\LibraryHooks;
 
+use VCR\Response;
+
 /**
  * Test if intercepting http/https using stream wrapper works.
  */
@@ -14,20 +16,40 @@ class StreamWrapperHookTest extends \PHPUnit_Framework_TestCase
 
     public function testEnable()
     {
+        $streamWrapper = new StreamWrapperHook();
+
         $testClass = $this;
-        $this->streamWrapper = new StreamWrapperHook();
-        $this->streamWrapper->enable(function ($request) use ($testClass) {
+        $streamWrapper->enable(function ($request) use ($testClass) {
         });
     }
 
     public function testDisable()
     {
-        $testClass = $this;
-        $this->streamWrapper = new StreamWrapperHook();
-        $this->streamWrapper->disable();
+        $streamWrapper = new StreamWrapperHook();
+        $streamWrapper->disable();
     }
 
-    public function tearDown()
+    public function testSeek()
     {
+        $hook = new StreamWrapperHook();
+        $hook->enable(function ($request) {
+            return new Response(200, array(), 'A Test');
+        });
+        $hook->stream_open('http://example.com', 'r', 0, $openedPath);
+
+        $this->assertFalse($hook->stream_seek(-1, SEEK_SET));
+        $this->assertTrue($hook->stream_seek(0, SEEK_SET));
+        $this->assertEquals('A', $hook->stream_read(1));
+
+        $this->assertFalse($hook->stream_seek(-1, SEEK_CUR));
+        $this->assertTrue($hook->stream_seek(1, SEEK_CUR));
+        $this->assertEquals('Test', $hook->stream_read(4));
+
+        $this->assertFalse($hook->stream_seek(-1000, SEEK_END));
+        $this->assertTrue($hook->stream_seek(-4, SEEK_END));
+        $this->assertEquals('Test', $hook->stream_read(4));
+
+        // invalid whence
+        $this->assertFalse($hook->stream_seek(0, -1));
     }
 }
