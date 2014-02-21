aaafc00d78eaa5018060051a5360a4f23861c5d0
Adrian Philipp <mail@adrian-philipp.com>
2014-02-21 17:41:48 +0100

    Cleanup: Adds docblocks to storage classes.

--- src/VCR/Storage/Storage.php
+++ src/VCR/Storage/Storage.php
@@ -5,6 +5,8 @@ namespace VCR\Storage;
 interface Storage extends \Iterator
 {
     /**
+     * Stores an array of data.
+     *
      * @return void
      */
     public function storeRecording(array $recording);
--- src/VCR/Storage/Yaml.php
+++ src/VCR/Storage/Yaml.php
@@ -6,19 +6,54 @@ use Symfony\Component\Yaml\Parser;
 use Symfony\Component\Yaml\Dumper;
 use VCR\Util\Assertion;
 
+/**
+ * Yaml based storage for records.
+ *
+ * This storage can be iterated while keeping the memory consumption to the
+ * amount of memory used by the largest record.
+ */
 class Yaml implements Storage
 {
-    const STATUS_IN_OBJECT = true;
-    const STATUS_NOT_IN_OBJECT = false;
+    /**
+     * @var resource File handle.
+     */
+    protected $handle;
 
-    private $handle;
-    private $filePath;
-    private $recording;
-    private $yamlDumper;
-    private $yamlParser;
-    private $isEOF = false;
-    private $valid = true;
+    /**
+     * @var array Current parsed record.
+     */
+    protected $current;
 
+    /**
+     * @var integer Number of the current recording.
+     */
+    protected $position = 0;
+
+    /**
+     * @var boolean True when parser is at the end of the file.
+     */
+    protected $isEOF = false;
+
+    /**
+     * @var boolean If the current position is valid.
+     */
+    protected $valid = true;
+
+    /**
+     * @var Symfony\Component\Yaml\Parser Yaml parser.
+     */
+    protected $yamlParser;
+
+    /**
+     * @var  Symfony\Component\Yaml\Dumper Yaml writer.
+     */
+    protected $yamlDumper;
+
+    /**
+     * Creates a new YAML based file store.
+     *
+     * @param string $filePath Path to a file, will be created if not existing.
+     */
     public function __construct($filePath, Parser $parser = null, Dumper $dumper = null)
     {
         if (!file_exists($filePath)) {
@@ -30,11 +65,13 @@ class Yaml implements Storage
         Assertion::writeable($filePath, "Specified path '{$filePath}' must be writeable.");
 
         $this->handle = fopen($filePath, 'r+');
-        $this->filePath = $filePath;
         $this->yamlParser = $parser ?: new Parser();
         $this->yamlDumper = $dumper ?: new Dumper();
     }
 
+    /**
+     * @inheritDoc
+     */
     public function storeRecording(array $recording)
     {
         fseek($this->handle, -1, SEEK_END);
@@ -42,28 +79,50 @@ class Yaml implements Storage
         fflush($this->handle);
     }
 
+    /**
+     * Returns the current record.
+     *
+     * @return array Parsed current record.
+     */
     public function current()
     {
-        return $this->recording;
+        return $this->current;
     }
 
+    /**
+     * Returns the current key.
+     *
+     * @return void
+     */
     public function key()
     {
+        return $this->position;
     }
 
+    /**
+     * Parses the next record.
+     *
+     * @return void
+     */
     public function next()
     {
-        $recording = $this->yamlParser->parse($this->readNextRecording());
-        $this->recording = $recording[0];
+        $recording = $this->yamlParser->parse($this->readNextRecord());
+        $this->current = $recording[0];
+        ++$this->position;
     }
 
-    private function readNextRecording()
+    /**
+     * Returns the next record in raw format.
+     *
+     * @return string Next record in raw format.
+     */
+    private function readNextRecord()
     {
         if ($this->isEOF) {
             $this->valid = false;
         }
 
-        $status = self::STATUS_NOT_IN_OBJECT;
+        $isInRecord = false;
         $recording = '';
         $lastChar = null;
 
@@ -71,17 +130,17 @@ class Yaml implements Storage
             $isNewArrayStart = ($char === '-') && ($lastChar === PHP_EOL || $lastChar === null);
             $lastChar = $char;
 
-            if ($status === self::STATUS_IN_OBJECT && $isNewArrayStart) {
-                $status = self::STATUS_NOT_IN_OBJECT;
+            if ($isInRecord && $isNewArrayStart) {
+                $isInRecord = false;
                 fseek($this->handle, -1, SEEK_CUR);
                 break;
             }
 
-            if ($status === self::STATUS_NOT_IN_OBJECT && $isNewArrayStart) {
-                $status = self::STATUS_IN_OBJECT;
+            if (!$isInRecord && $isNewArrayStart) {
+                $isInRecord = true;
             }
 
-            if ($status === self::STATUS_IN_OBJECT) {
+            if ($isInRecord) {
                 $recording .= $char;
             }
         }
@@ -93,25 +152,38 @@ class Yaml implements Storage
         return $recording;
     }
 
+    /**
+     * Resets the storage to the beginning.
+     *
+     * @return void
+     */
     public function rewind()
     {
         rewind($this->handle);
         $this->isEOF = false;
         $this->valid = true;
+        $this->position = 0;
     }
 
+    /**
+     * Returns true if the current record is valid.
+     *
+     * @return boolean True if the current record is valid.
+     */
     public function valid()
     {
-        if (is_null($this->recording)) {
+        if (is_null($this->current)) {
             $this->next();
         }
 
-        return ! is_null($this->recording) && $this->valid;
+        return ! is_null($this->current) && $this->valid;
     }
 
+    /**
+     * Closes file handle.
+     */
     public function __destruct()
     {
         fclose($this->handle);
     }
-
 }
