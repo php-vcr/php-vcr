e2a400cdf3169a89e7fd37588b06969ac2a66b84
David Buchmann <david.buchmann@liip.ch>
2015-02-23 14:47:51 +0100

    support path separators in cassette names

---
+++ tests/VCR/Storage/AbstractStorageTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace VCR\Storage;
+
+use org\bovigo\vfs\vfsStream;
+
+/**
+ * Test integration of PHPVCR with PHPUnit.
+ */
+class AbstractStorageTest extends \PHPUnit_Framework_TestCase
+{
+    protected $handle;
+    protected $filePath;
+    protected $storage;
+
+    public function testFilePathCreated()
+    {
+        $fs = vfsStream::setup('test');
+
+        $this->storage = new TestStorage(vfsStream::url('test/'), 'file');
+        $this->assertTrue($fs->hasChild('file'));
+
+        $this->storage = new TestStorage(vfsStream::url('test/'), 'folder/file');
+        $this->assertTrue($fs->hasChild('folder'));
+        $this->assertTrue($fs->getChild('folder')->hasChild('file'));
+    }
+
+    /**
+     * @expectedException \VCR\VCRException
+     */
+    public function testRootNotExisting()
+    {
+        vfsStream::setup('test');
+        $this->storage = new TestStorage(vfsStream::url('test/foo'), 'file');
+    }
+
+}
+
+class TestStorage extends AbstractStorage
+{
+    private $recording;
+
+    public function storeRecording(array $recording)
+    {
+        $this->recording = $recording;
+    }
+
+    public function next()
+    {
+        list ($this->position, $this->current) = each ($this->recording);
+
+        return $this->current;
+    }
+
+    public function valid()
+    {
+        return (boolean) $this->position;
+    }
+
+    public function rewind()
+    {
+        reset($this->recording);
+    }
+}
