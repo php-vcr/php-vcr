d803d9b4d6bdb9970f6db39e7261dbe6d7b36445
Tyler Collier <git@tylercollier.com>
2015-03-01 19:09:49 -0700

    Fix URL tests, add host matcher and tests

--- tests/VCR/RequestMatchers/HostMatcherTest.php
+++ tests/VCR/RequestMatchers/HostMatcherTest.php
@@ -4,12 +4,12 @@ namespace VCR\RequestMatchers;
 
 use \VCR\Request;
 
-class UrlMatcherTest extends RequestMatcherTestCase
+class HostMatcherTest extends RequestMatcherTestCase
 {
 	private $matcher;
 
 	public function setUp() {
-		$this->matcher = new UrlMatcher();
+		$this->matcher = new HostMatcher();
 	}
 
     public function testMatch()
@@ -20,22 +20,17 @@ class UrlMatcherTest extends RequestMatcherTestCase
         $this->assertTrue($this->matcher->match($first, $second));
 
         $first = new Request('GET', 'http://example.com/first/path', array());
-        $second = new Request('GET', 'http://example.com/second/path', array());
-
-        $this->assertFalse($this->matcher->match($first, $second));
-
-        $first = new Request('GET', 'http://example.com/second', array());
-        $second = new Request('GET', 'http://example.com/second/path', array());
+        $second = new Request('GET', 'http://elpmaxe.com/second/path', array());
 
         $this->assertFalse($this->matcher->match($first, $second));
     }
 
     public function testGetMismatchMessage() {
     	$first = new Request('GET', 'http://example.com/first/path', array());
-        $second = new Request('GET', 'http://example.com/second/path', array());
+        $second = new Request('GET', 'http://elpmaxe.com/second/path', array());
 
         $mismatchMessage = $this->matcher->getMismatchMessage($first, $second);
-        $expectedMessage = $this->buildSimpleExpectedMessage('URL', 'http://example.com/first/path', 'http://example.com/second/path');
+        $expectedMessage = $this->buildSimpleExpectedMessage('Host', 'example.com', 'elpmaxe.com');
         $this->assertEquals($mismatchMessage, $expectedMessage);
     }
 }
