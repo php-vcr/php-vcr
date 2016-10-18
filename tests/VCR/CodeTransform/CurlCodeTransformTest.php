70a2158dd198372cdba1cb7bc8b21fc92f194e18
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2016-10-18 15:26:19 +0200

    Pumping proxy-object version

--- tests/VCR/CodeTransform/CurlCodeTransformTest.php
+++ tests/VCR/CodeTransform/CurlCodeTransformTest.php
@@ -59,7 +59,6 @@ class CurlCodeTransformTest extends \PHPUnit_Framework_TestCase
             array('$object->curl_multi_info_read(', '$object->curl_multi_info_read('),
 
             array('function send_http_asynchronous_curl_exec(', 'function send_http_asynchronous_curl_exec(')
-
         );
     }
 }
