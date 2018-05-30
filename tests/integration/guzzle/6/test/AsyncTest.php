13872a0b291c1a398461ac178850837648998271
David Négrier <d.negrier@thecodingmachine.com>
2018-05-30 17:10:47 +0200

    Fix curl reset (#248)
    ### Context
    
    When using PHP-VCR with Guzzle 6 with async requests, the first request is playing correctly but the next request is putting Guzzle in an infinite loop.
    
    It took me a whole day but I traced back this problem to the way the "curl_reset" hook is written. It clears the request but not the response. Hence, when another request is performed (even on a different URL) the same response is sent. This weird behaviour is causing an infinite loop in Guzzle 6.
    
    This problem is probably exactly the same as the issue #211. This PR should fix it.
    
    ### What has been done
    
    The first commit of this PR contains only the failing test (to showcase the problem). You see that the test never completes (because of the infinite loop): https://travis-ci.org/php-vcr/php-vcr/jobs/366140696
    
    The second commit is the fix. I simply added one line in the curl_reset hook to remove the response as well as the request.
    
    Note: this PR is based on the branch from PR #246 since I need unit tests from Guzzle 6 that are not yet merged.

---
+++ tests/integration/guzzle/6/test/AsyncTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace VCR\Example;
+
+use GuzzleHttp\Client;
+use org\bovigo\vfs\vfsStream;
+
+/**
+ * Tests example request.
+ */
+class AsyncTest extends \PHPUnit_Framework_TestCase
+{
+    const TEST_GET_URL = 'http://api.chew.pro/trbmb';
+    const TEST_GET_URL_2 = 'http://api.chew.pro/trbmb?foo=42';
+
+    public function setUp()
+    {
+        vfsStream::setup('testDir');
+        \VCR\VCR::configure()->setCassettePath(vfsStream::url('testDir'));
+    }
+
+    public function testAsyncLock()
+    {
+        \VCR\VCR::turnOn();
+        \VCR\VCR::insertCassette('test-cassette.yml');
+
+        $client = new Client();
+        $promise = $client->getAsync(self::TEST_GET_URL);
+        $response = $promise->wait();
+        $promise = $client->getAsync(self::TEST_GET_URL_2);
+        $promise->wait();
+        // Let's check that we can perform 2 async request on different URLs without locking.
+        // Solves https://github.com/php-vcr/php-vcr/issues/211
+
+        $this->assertValidGETResponse(\GuzzleHttp\json_decode($response->getBody()));
+
+        \VCR\VCR::turnOff();
+    }
+
+    protected function assertValidGETResponse($info)
+    {
+        $this->assertInternalType('array', $info, 'Response is not an array.');
+        $this->assertArrayHasKey('0', $info, 'API did not return any value.');
+    }
+}
