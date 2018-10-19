62f62ec84e4cdd87c71a0aa9aa04c64ad90d9b75
David Négrier <d.negrier@thecodingmachine.com>
2018-10-19 16:40:02 +0200

    Adding failing test for Opcache

---
+++ tests/integration/apache/www/performRequest.php
@@ -0,0 +1,7 @@
+<?php
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'http://localhost/tests/integration/apache/www/target.html');
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_exec($ch);
+curl_close($ch);
