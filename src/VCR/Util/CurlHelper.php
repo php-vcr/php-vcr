ea731f9204410d2cc8755c94b3c9b5e2fda711ff
Cole Shirley <71522167+cole-flexsim@users.noreply.github.com>
2021-04-03 19:43:17 -0600

    fix issue with a "SOAPAction;" header

--- src/VCR/Util/CurlHelper.php
+++ src/VCR/Util/CurlHelper.php
@@ -173,6 +173,10 @@ class CurlHelper
                     if (!isset($headerParts[1])) {
                         $headerParts[0] = rtrim($headerParts[0], ':');
                         $headerParts[1] = '';
+                        if (strpos($headerParts[0], ';') !== false) {       // new lines here
+                            $headerParts[0] = rtrim($headerParts[0], ';');
+                            $headerParts[1] = ';';
+                        }  
                     }
                     $request->setHeader($headerParts[0], $headerParts[1]);
                 }
