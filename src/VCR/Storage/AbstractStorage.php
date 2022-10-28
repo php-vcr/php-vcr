caf8d41990a8e66e3dd1160c03c65cba4ecb5528
William Ford <william.f@covergenius.com>
2022-10-28 11:16:41 +1100

    Added return types for 8-1 compatibility Iterator implementations

--- src/VCR/Storage/AbstractStorage.php
+++ src/VCR/Storage/AbstractStorage.php
@@ -68,7 +68,7 @@ abstract class AbstractStorage implements Storage
      *
      * @return array<string,mixed>|null parsed current record
      */
-    public function current()
+    public function current(): mixed
     {
         return $this->current;
     }
