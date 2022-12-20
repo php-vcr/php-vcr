1d8349f4ba9d56f28fef3dee33a301c0c0b454ee
oranges13 <oranges13@users.noreply.github.com>
2022-12-20 14:26:02 -0500

    Add CURLINFO_APPCONNECT_TIME to CurlHelper
    Fixes #309

--- src/VCR/Util/CurlHelper.php
+++ src/VCR/Util/CurlHelper.php
@@ -35,6 +35,7 @@ class CurlHelper
         \CURLINFO_CONTENT_LENGTH_DOWNLOAD => 'download_content_length',
         \CURLINFO_CONTENT_LENGTH_UPLOAD => 'upload_content_length',
         \CURLINFO_CONTENT_TYPE => 'content_type',
+        \CURLINFO_APPCONNECT_TIME => 'appconnect_time',
     ];
 
     /**
@@ -106,6 +107,7 @@ class CurlHelper
                 $info = mb_strlen(HttpUtil::formatAsStatusWithHeadersString($response), 'ISO-8859-1');
                 break;
             case \CURLPROXY_HTTPS:
+            case \CURLINFO_APPCONNECT_TIME:
                 $info = '';
                 break;
             default:
